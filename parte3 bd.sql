6c607dadad782a31922aa1945e416ee6be5bc767
ArturAlmeidaGama <141968463+ArturAlmeidaGama@users.noreply.github.com>
2023-11-30 15:30:01 -0300

    Update parte3 bd.sql

--- parte3 bd.sql
+++ parte3 bd.sql
@@ -4,4 +4,4 @@ select tb_pedido_quitacao.id_pedido,tb_pedido.numero_pedido,tb_fornecedor.cnpj,t
 from tb_pedido
 inner join tb_pedido_quitacao on tb_pedido.id=tb_pedido_quitacao.id_pedido
 inner join tb_fornecedor on tb_pedido.id_fornecedor=tb_fornecedor.id
-where data_vencimento>='2023-09-23'
+where data_vencimento>='2023-09-30'
