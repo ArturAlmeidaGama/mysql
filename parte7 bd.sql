f347fb159954aa186d3311bc575732abecb9c5ef
ArturAlmeidaGama <141968463+ArturAlmeidaGama@users.noreply.github.com>
2023-11-30 15:31:36 -0300

    Update parte7 bd.sql

--- parte7 bd.sql
+++ parte7 bd.sql
@@ -1,2 +1,7 @@
-use db_contas_a_pagar
+use db_contas_a_pagar;
 
+select monthname(tb_pedido_quitacao.data_pagamento) as mes,sum(tb_pedido_quitacao.valor_pago) as total_pago,avg(tb_pedido_quitacao.valor_pago) as media
+from tb_pedido_quitacao
+inner join tb_pedido on tb_pedido_quitacao.id_pedido=tb_pedido.id
+where year(tb_pedido.data_vencimento)=2023
+group by monthname(tb_pedido_quitacao.data_pagamento)
