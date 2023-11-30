d6fb97d5965179a13dff713def31319458b710e6
ArturAlmeidaGama <141968463+ArturAlmeidaGama@users.noreply.github.com>
2023-11-30 15:29:37 -0300

    Update parte2 bd.sql

--- parte2 bd.sql
+++ parte2 bd.sql
@@ -1,7 +1,7 @@
 use db_contas_a_pagar;
 
-select f.id,f.cnpj,f.razao_social,fc.email,fc.telefone,tc.descricao
-from tb_fornecedor f
-left join tb_fornecedor_contato fc on f.id=fc.id_fornecedor
-left join tb_tipo_contato tc on fc.id_tipo_contato=tc.id
-where fc.id_fornecedor is null
+select tb_fornecedor.id,tb_fornecedor.cnpj,tb_fornecedor.razao_social,tb_fornecedor_contato.email,tb_fornecedor_contato.telefone,tb_tipo_contato.descricao
+from tb_fornecedor
+left join tb_fornecedor_contato on tb_fornecedor.id=tb_fornecedor_contato.id_fornecedor
+left join tb_tipo_contato on tb_fornecedor_contato.id_tipo_contato=tb_tipo_contato.id
+where tb_fornecedor_contato.id_fornecedor is null
