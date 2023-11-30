002503dd415e17b2a102b5f14c84daf9635f08a5
ArturAlmeidaGama <141968463+ArturAlmeidaGama@users.noreply.github.com>
2023-11-30 15:31:15 -0300

    Update parte6 bd.sql

--- parte6 bd.sql
+++ parte6 bd.sql
@@ -1,3 +1,9 @@
 use db_contas_a_pagar;
 
-select tb_fornecedor.cnpj,tb_fornecedor.razao_social,tb_pedido_quitacao.valor_pago,tb_pedido.valor_pedido
+select tb_fornecedor.cnpj,tb_fornecedor.razao_social,sum(tb_pedido.valor_pedido) as valor_a_pagar
+from tb_fornecedor
+left join tb_pedido on tb_fornecedor.id=tb_pedido.id_fornecedor
+left join tb_pedido_quitacao on tb_pedido.id=tb_pedido_quitacao.id_pedido
+where year(tb_pedido.data_vencimento)=2023
+and tb_pedido_quitacao.valor_pago is null
+group by tb_fornecedor.cnpj,tb_fornecedor.razao_social;
