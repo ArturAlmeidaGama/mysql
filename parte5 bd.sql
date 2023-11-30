380d3f4feea51ab7401ef7a7519139aa04948d7a
ArturAlmeidaGama <141968463+ArturAlmeidaGama@users.noreply.github.com>
2023-11-30 15:30:49 -0300

    Update parte5 bd.sql

--- parte5 bd.sql
+++ parte5 bd.sql
@@ -1,2 +1,8 @@
 use db_contas_a_pagar;
 
+select tb_fornecedor.cnpj,tb_fornecedor.razao_social,sum(tb_pedido_quitacao.valor_pago) as total_pago,avg(tb_pedido_quitacao.valor_pago) as media
+from tb_pedido
+inner join tb_pedido_quitacao on tb_pedido.id=tb_pedido_quitacao.id_pedido
+inner join tb_fornecedor on tb_pedido.id_fornecedor=tb_fornecedor.id
+where year(tb_pedido.data_vencimento)=2023
+group by tb_fornecedor.cnpj,tb_fornecedor.razao_social;
